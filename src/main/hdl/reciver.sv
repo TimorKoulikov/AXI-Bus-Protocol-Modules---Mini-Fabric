539d8ebc916dc5acd9bb9cc3c1e5f64ac0c50330
Timor Koulikov <t_koulikov@apple.com>
2026-03-09 15:56:10 +0200

    modifing datatypes

--- src/main/hdl/reciver.sv
+++ src/main/hdl/reciver.sv
@@ -1,17 +1,25 @@
 
+/*=======================
+module is reciving data from a specific channel of axi component.
+output - the most recent data from axi component
+=========================*/
+
 /*TODO:
 	1.) check how clk works in reciver
 	2.) check if need to add reset
 */
+
 module reciveri(
 clk,		//axi component clk
 data_in. 	//AXI channel that comes from axi component
-
-data_out,	//
+data_out,	//data of entering valid and new data.
 nsp		//new data pulse (width of signal is 1 clk)
 );
+//-----imports-----
+import ../resources/*
 //-----Parameters-----
-parameter CHANNEL_TYPE="AW",
+
+parameter CHANNEL_TYPE=AW,
 BUS_WIDTH=128
 
 //-----Input Ports-----
--- src/main/resources/axi_datatypes.sv
+++ src/main/resources/axi_datatypes.sv
@@ -1,5 +1,7 @@
 package axi_datatypes;
 
+typedef enum {AW,AR,W,R,B} axiChannelTypes;
+
 struct {
 	wire clk,
 	wire [2:0] BURST,
