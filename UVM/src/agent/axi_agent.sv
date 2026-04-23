060dca227028654c6394470ee3be71c14852dc32
Lab_timor <epagtk@vlsi-ri57.technion.ac.il>
2026-04-23 14:05:24 +0300

    [ADJUST2FABRIC]: adjust axi_agent for fabric uvm

--- UVM/src/agent/axi_agent.sv
+++ UVM/src/agent/axi_agent.sv
@@ -31,10 +31,11 @@
 class axi_agent extends uvm_agent;
 
      `uvm_component_utils(axi_agent)
-
-     axi_driver                         axi_drv;
-     axi_monitor                        axi_mon;
-     axi_sequencer                      axi_seqr;
+	
+	// TODO : change to parametric arrtibue. this should be num of masters
+     axi_driver                         axi_drv[5];
+     axi_monitor                        axi_mon[5];
+     axi_sequencer                      axi_seqr[5];
 
      // Virtual interface for the AXI bus BFM.
      // Must be provided by the TB via uvm_config_db under key "axi_vif".
@@ -46,31 +47,35 @@ class axi_agent extends uvm_agent;
 
      function void build_phase(uvm_phase phase);
 
-          super.build_phase(phase);
+		super.build_phase(phase);
 
-          // Critical binding point:
-          // Keeps the agent hierarchy-independent. If this fails, the env/top didn’t set axi_vif.
-          if (!uvm_config_db#(virtual axi_if)::get(this, "", "axi_vif", vif))
-               `uvm_fatal("AXI_AGENT", "No virtual interface bound to axi_agent")
+       	
+		for(int i=0;i<5;i++) begin
+			
+			// Critical binding point:
+			// Keeps the agent hierarchy-independent. If this fails, the env/top didn’t set axi_vif.
+			if (!uvm_config_db#(virtual axi_if)::get(this, "", $sformatf("axi_vif_%0d", i), vif))
+				`uvm_fatal("AXI_AGENT", "No virtual interface bound to axi_agent")
+			
+        	// Create sub-components (behavior is in driver/monitor, not here).
+       		axi_drv[i]                        = axi_driver     ::type_id::create($sformatf("axi_drv_%0d", i), this);
+        	axi_mon[i]                        = axi_monitor    ::type_id::create($sformatf("axi_mon_%0d", i), this);
+        	axi_seqr[i]                       = axi_sequencer  ::type_id::create($sformatf("axi_seqr_%0d", i), this);
 
-          // Create sub-components (behavior is in driver/monitor, not here).
-          axi_drv                        = axi_driver     ::type_id::create("axi_drv", this);
-          axi_mon                        = axi_monitor    ::type_id::create("axi_mon", this);
-          axi_seqr                       = axi_sequencer  ::type_id::create("axi_seqr", this);
-
-          // Pass VIF down so sub-components don’t depend on hierarchical paths.
-          uvm_config_db#(virtual axi_if)::set(this, "axi_drv", "axi_vif", vif);
-          uvm_config_db#(virtual axi_if)::set(this, "axi_mon", "axi_vif", vif);
-
+        	// Pass VIF down so sub-components don’t depend on hierarchical paths.
+        	uvm_config_db#(virtual axi_if)::set(this, $sformatf("axi_drv_%0d", i), "axi_vif", vif);
+        	uvm_config_db#(virtual axi_if)::set(this, $sformatf("axi_mon_%0d", i), "axi_vif", vif);
+		end
      endfunction
 
      function void connect_phase(uvm_phase phase);
 
-          super.connect_phase(phase);
-
-          // Allows sequences to control the AXI driver (e.g., responder policy, delays).
-          // If you later support passive-only mode, guard this behind is_active==UVM_ACTIVE.
-          axi_drv.seq_item_port.connect(axi_seqr.seq_item_export);
+     	super.connect_phase(phase);
+		for(int i=0;i<5;i++) begin
+	        // Allows sequences to control the AXI driver (e.g., responder policy, delays).
+	        // If you later support passive-only mode, guard this behind is_active==UVM_ACTIVE.
+	        axi_drv[i].seq_item_port.connect(axi_seqr[i].seq_item_export);
+		end
 		
      endfunction
 
